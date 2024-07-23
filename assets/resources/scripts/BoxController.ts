b17dc9b3a080c60864d635b2900b1961c16ca414
Leo Cheung <leozhang621@gmail.com>
2024-07-23 13:50:34 +0800

    add player prefab

---
+++ assets/resources/scripts/BoxController.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('BoxController')
+export class BoxController extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
